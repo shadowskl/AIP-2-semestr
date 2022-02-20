1ac66480d5e79610fbde0068874ab5d4d24128a4
Vadim <vadyasokolovskiy@gmail.com>
2022-02-20 21:58:30 +0300

    Week_1-2_Zadanie_1-3

---
+++ Week_1-2/Week_1-2_Zadanie-2/Week_1-2_Zadanie-2.cpp
@@ -0,0 +1,19 @@
+﻿//2. Пользователь вводит параметры фигуры. Найти периметр и площадь фигур:  f. Салинон
+
+#include <iostream>
+#include <cmath>
+
+#define _USE_MATH_DEFINES
+
+int main()
+{
+
+    float r1, r2, S;
+    float M_PI = 3.14;
+    std::cout << "enter two radii\n";
+    std::cin >> r1 >> r2;
+    S = 0.25 * M_PI * ((r1 + r2) * (r1 + r2));
+    std::cout << "answer:\t" << S;
+
+    std::getchar();
+}
---
+++ Week_1-2/Week_1-2_Zadanie-3/Week_1-2_Zadanie-3.cpp
@@ -0,0 +1,10 @@
+﻿//3.Пользователь вводит два числа. Необходимо поменять значения переменных так, чтобы значение первой оказалось во второй, а второй - в первой.
+#include <iostream>
+int main() {
+    float Number1, Number2;
+    std::cout << "Enter two numbers\n";
+    std::cin >> Number1 >> Number2;
+    std::swap(Number1, Number2);
+    std::cout << "Number1: " << Number1 << "\t"  << "Number2: " << Number2;
+    std::getchar();
+}
