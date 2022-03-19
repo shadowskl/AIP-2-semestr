32b63b0568449f81218a2e4b220adeef028e790d
Vadim <vadyasokolovskiy@gmail.com>
2022-03-19 22:08:51 +0300

    Zadanie_4

---
+++ Week_1-2/Week_1-2_Zadanie-4/Week_1-2_Zadanie-4.cpp
@@ -0,0 +1,12 @@
+﻿//4.Вычислите значение выражения:√ (x^2 + b)−[b^2*sin^3(x + a)] / x при a = 0.1, b = 0.2 и x = 1
+
+#include <iostream>
+#include <cmath> 
+
+int main()
+{
+    float a = 0.1, b = 0.2, x = 1, Answer=1;
+    Answer = sqrt((x*x)+b)-(((b*b)*(sin(x + a) * sin(x + a) * sin(x + a))/x));
+    std::cout << Answer;
+    std::getchar();
+}
