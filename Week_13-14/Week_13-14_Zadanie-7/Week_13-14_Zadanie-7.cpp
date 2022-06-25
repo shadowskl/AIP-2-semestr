e3aecb74c5e04fca12ae2c18ea256cc0e2f83ecb
Vadim <vadyasokolovskiy@gmail.com>
2022-06-25 14:36:34 +0300

    Update Week_13-14_Zadanie-7.cpp

--- Week_13-14/Week_13-14_Zadanie-7/Week_13-14_Zadanie-7.cpp
+++ Week_13-14/Week_13-14_Zadanie-7/Week_13-14_Zadanie-7.cpp
@@ -20,19 +20,18 @@ struct Person {
 
 Person people[10];
 
-
 int main()
 {
-	people[1].surname = " Davis";
-	people[2].surname = " Gonzalez";
-	people[3].surname = "  Smith";
-	people[4].surname = "  Williams";
-	people[5].surname = "  Boyd";
-	people[6].surname = "  Baker";
-	people[7].surname = "  Stanley";
-	people[8].surname = "  Hoffman";
-	people[9].surname = "  Arnold";
-	people[10].surname = "  Rice";
+	people[0].surname = " Davis";
+	people[1].surname = " Gonzalez";
+	people[2].surname = "  Smith";
+	people[3].surname = "  Williams";
+	people[4].surname = "  Boyd";
+	people[5].surname = "  Baker";
+	people[6].surname = "  Stanley";
+	people[7].surname = "  Hoffman";
+	people[8].surname = "  Arnold";
+	people[9].surname = "  Rice";
 	int MaxHeight = 0;
 	float SrAge = 0;
 	for (int i = 0; i < 10; i++)
